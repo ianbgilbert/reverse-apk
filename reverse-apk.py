726dd8f9d200efe404cb921b5f8d996b83539e6e
ianbgilbert <ianbgilbert@gmail.com>
2015-11-19 20:41:18 -0500

    Grammar and spelling corrections

--- reverse-apk.py
+++ reverse-apk.py
@@ -13,12 +13,12 @@ apk_file = sys.argv[1]
 # Download apktool:
 if not os.path.isfile("apktool.jar"):
 	print("We will now download apktool to unpack the APK.")
-	print("It will downloaded from https://bitbucket.org/iBotPeaches/apktool/downloads/apktool_2.0.0rc2.jar.")
+	print("It will download from https://bitbucket.org/iBotPeaches/apktool/downloads/apktool_2.0.0rc2.jar.")
 	print("You can choose to stop the script here and download apktool yourself.")
-	print("The tool must be named apktool.jar and placed in the same directory as this script.")
+	print("The tool must be named apktool.jar and be placed in the same directory as this script.")
 	answer = ''
 	while answer!='n' and answer!='y':
-		answer = input("Do tou wish to continue and download apktool? [y/n] ").lower()
+		answer = input("Do you wish to continue and download apktool? [y/n] ").lower()
 	if answer=='n':
 		sys.exit(0)
 	os.system("wget -qO apktool.jar https://bitbucket.org/iBotPeaches/apktool/downloads/apktool_2.0.0rc2.jar")
@@ -31,12 +31,12 @@ os.system("java -jar apktool.jar d " + apk_file + " -fo apk-dir")
 print()
 if not os.path.isfile("smali.jar"):
 	print("We will now download Smali to convert the .smali files into a .dex file.")
-	print("It will downloaded from https://bitbucket.org/JesusFreke/smali/downloads/smali-2.0.3.jar.")
+	print("It will download from https://bitbucket.org/JesusFreke/smali/downloads/smali-2.0.3.jar.")
 	print("You can choose to stop the script here and download Smali yourself.")
-	print("The tool must be named smali.jar and placed in the same directory as this script.")
+	print("The tool must be named smali.jar and be placed in the same directory as this script.")
 	answer = ''
 	while answer!='n' and answer!='y':
-		answer = input("Do tou wish to continue and download Smali? [y/n] ").lower()
+		answer = input("Do you wish to continue and download Smali? [y/n] ").lower()
 	if answer=='n':
 		sys.exit(0)
 	os.system("wget -qO smali.jar https://bitbucket.org/JesusFreke/smali/downloads/smali-2.0.3.jar")
@@ -49,12 +49,12 @@ os.system("java -jar smali.jar apk-dir/smali")
 print()
 if not os.path.isfile("dex2jar.zip"):
 	print("We will now download dex2jar 0.0.9.15 to convert the .dex file into a .jar file.")
-	print("It will downloaded from https://dex2jar.googlecode.com/files/dex2jar-0.0.9.15.zip.")
+	print("It will download from https://dex2jar.googlecode.com/files/dex2jar-0.0.9.15.zip.")
 	print("You can choose to stop the script here and download dex2jar yourself.")
-	print("The tool must be named dex2jar.zip and placed in the same directory as this script.")
+	print("The tool must be named dex2jar.zip and be placed in the same directory as this script.")
 	answer = ''
 	while answer!='n' and answer!='y':
-		answer = input("Do tou wish to continue and download dex2jar? [y/n] ").lower()
+		answer = input("Do you wish to continue and download dex2jar? [y/n] ").lower()
 	if answer=='n':
 		sys.exit(0)
 	os.system("wget -qO dex2jar.zip https://dex2jar.googlecode.com/files/dex2jar-0.0.9.15.zip")
